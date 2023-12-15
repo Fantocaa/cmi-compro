7d389ffe475505e03e9179c081deb30bf112e9dc
Fantoca.Tako <fantoca@tako.co.id>
2023-12-15 16:49:19 +0700

    update 0.1

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import navbar from "../components/static/navbar.vue";
+
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <navbar />
+    <!-- <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a> -->
+    <div class="flex justify-between">
+      <h1>Home</h1>
+      <button>meong</button>
+    </div>
+  </div>
+</template>
